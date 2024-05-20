daf3d047a9044b40cd6665a602728734b8370182
kudo153759 <111909214+kudo153759@users.noreply.github.com>
2024-05-21 02:59:04 +0800

    new
    aas

---
+++ messages.js
@@ -0,0 +1,6 @@
+const anniversaryDate = 'May 2, 2024 00:00:00' 
+const noMessage = 'As you start this beautiful new day, may your heart always be filled with gratitude. Good morning, my everything.'
+const endMessage = '<br><br><br>Love,<br>Babii'
+const messages = {
+    '10:11': 'Sample message'
+}
